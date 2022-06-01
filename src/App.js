d264779deb45d1b1f12601323a3bd8c96d63c3d2
davon <davonwhitworth@gmail.com>
2022-05-31 17:39:14 -0700

    card end, no img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import "./App.css";
+import "./styles/App.css";
 import Projectcard from "./components/projectcard";
 
 function App() {
   return (
-    <Projectcard
-      title="My project"
-      languages="reactJS html javascript"
-      description="This is my page to show off my garbage development skills"
-    />
+    <>
+      <Projectcard
+        title="Pop"
+        languages="Unity"
+        description="A web game I produced with my 2 brothers. A peacful top-down shoot which synchronizes the enemy waves with it's music track."
+        imagesrc={"./projectpics/Poppic.PNG"}
+        imagealt="Pop Pic"
+        link="https://gage-langdon.com/pop/"
+      />
+    </>
   );
 }
 
--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -1,30 +1,60 @@
 import styled from "styled-components";
 
-// import portfoliopic from "./src/projectpics/porfoliopic.PNG";
-// <img src={portfoliopic} alt="Portfolio image" />;
-
 const Card = styled.div`
-  displaly: grid;
+  width: 60%;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
   color: white;
-  border-color: white;
+  border: 4px solid grey;
+  border-radius: 10px;
+  padding: 10px;
 `;
 
-const Title = styled.div`
-  font-size: 24;
+const Appinfo = styled.div`
+  margin: 5px;
+  padding: 5px;
+  line-height: 1.7;
 `;
 
-const Langs = styled.div``;
+const Apppic = styled.img`
+  margin: 5px;
+`;
 
-const Description = styled.div``;
-const ProjectLink = styled.link``;
+// const Apppic = styled.div`
+//   margin: 5px;
+// `;
+// <Apppic>
+//   <img src={props.imagesrc} alt={props.imagealt} />
+// </Apppic>;
+
+const Title = styled.a`
+  margin: 3px;
+  font-size: 1.5em;
+  color: inherit;
+`;
+
+const Langs = styled.div`
+  margin: 3px;
+`;
+
+const Description = styled.div`
+  margin: 3px;
+  line-height: 1.2;
+`;
 
 function projectcard(props) {
+  console.log(props.imagesrc);
   return (
     <>
       <Card>
-        <Title href="google.com">{props.title}</Title>
-        <Langs>{props.languages}</Langs>
-        <Description>{props.description} </Description>
+        <Apppic></Apppic>
+        <Appinfo>
+          <Title target="_blank" href={props.link}>
+            {props.title}
+          </Title>
+          <Langs>{props.languages}</Langs>
+          <Description>{props.description} </Description>
+        </Appinfo>
       </Card>
     </>
   );
