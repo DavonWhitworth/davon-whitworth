b50967f7ef7717c517c67a59cef9e88472681c7f
davon <davonwhitworth@gmail.com>
2022-06-16 20:33:09 -0700

    delete old information

--- src/components/projectlist.js
+++ src/components/projectlist.js
@@ -1,8 +1,8 @@
 import Projectcard from "./projectcard";
 import styled from "styled-components";
 import Poppicture from "../projectpics/Poppic.PNG";
-import Typingtestpicture from "../projectpics/typingtestpic.PNG";
-import Lightsitepicture from "../projectpics/lightsitepic.PNG";
+//import Typingtestpicture from "../projectpics/typingtestpic.PNG";
+//import Lightsitepicture from "../projectpics/lightsitepic.PNG";
 
 const Projectblock = styled.div`
   padding: 5px 30px 5px 30px;
@@ -28,22 +28,22 @@ const Projectlist = () => {
         languages="Unity"
         description="A web game I produced with my 2 brothers. A peacful top-down shooter which synchronizes the enemy waves with it's music track."
         imagesrc={Poppicture}
-        imagealt="Pop Pic"
+        imagealt="Pop picture"
         link="https://gage-langdon.com/pop/"
       />
       <Projectcard
         title="Vocabulary typing test"
         languages="React"
         description="A typing test that also helps you improve your vocabulary"
-        imagesrc={Typingtestpicture}
-        imagealt="Vocabulary typing test"
+        //imagesrc={Typingtestpicture}
+        imagealt="Vocabulary typing test picture"
       />
       <Projectcard
         title="Light sight"
         languages="React"
         description="A site where you can illuminate your beautiful face with the color of your choice using your screen"
-        imagesrc={Lightsitepicture}
-        imagealt="Light sight"
+        //imagesrc={Lightsitepicture}
+        imagealt="Light sight picture"
       />
     </Projectblock>
   );
